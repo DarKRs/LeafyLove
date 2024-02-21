7202ebd661b1ebfe85eca4ad8d223139c175663f
DarKRs <centralstreet_24@mail.ru>
2024-02-21 16:58:44 +0600

    Задержка для полива

--- ViewModels/PlantViewModel.cs
+++ ViewModels/PlantViewModel.cs
@@ -13,8 +13,11 @@ namespace LeafyLove.ViewModels
 {
     public class PlantViewModel : INotifyPropertyChanged
     {
+        private bool canWater = true; // Изначально полив доступен
+
         private DispatcherTimer tickTimer;
         private DispatcherTimer growthTimer;
+        private DispatcherTimer waterTimer;
 
         Random random = new Random();
 
@@ -41,21 +44,26 @@ namespace LeafyLove.ViewModels
             Plant = new Plant("Ваше растение");
 
             // Инициализация команд
-            WaterCommand = new RelayCommand(o => Plant.Water(), o => Plant.WaterLevel < 200);
+            WaterCommand = new RelayCommand(o => WaterPlant(), o => Plant.WaterLevel < 200 && canWater);
             FertilizeCommand = new RelayCommand(o => Plant.Fertilize());
             TreatCommand = new RelayCommand(o => Plant.RemovePests(), o => Plant.HasPests);
 
-            // Настройка таймера для тика растения
+            // Настройка таймера для роста растения
             growthTimer = new DispatcherTimer();
-            growthTimer.Interval = TimeSpan.FromSeconds(5);//TimeSpan.FromHours(1); // Установите интервал в соответствии с желаемой скоростью роста
+            growthTimer.Interval = TimeSpan.FromSeconds(20);//TimeSpan.FromHours(1); // Установите интервал в соответствии с желаемой скоростью роста
             growthTimer.Tick += GrowthTimer_Tick;
             growthTimer.Start();
 
             // Настройка таймера для тика растения
             tickTimer = new DispatcherTimer();
-            tickTimer.Interval = TimeSpan.FromSeconds(3);//TimeSpan.FromHours(1); // Установите интервал в соответствии с желаемой скоростью роста
+            tickTimer.Interval = TimeSpan.FromSeconds(10);//TimeSpan.FromHours(1); // Установите интервал в соответствии с желаемой скоростью роста
             tickTimer.Tick += TickTimer_Tick;
             tickTimer.Start();
+
+            // Настройка таймера задержки полива
+            waterTimer = new DispatcherTimer();
+            waterTimer.Interval = TimeSpan.FromSeconds(5); // Задержка 30 секунд между поливами
+            waterTimer.Tick += WaterTimer_Tick;
         }
 
         private void GrowthTimer_Tick(object sender, EventArgs e)
@@ -68,6 +76,24 @@ namespace LeafyLove.ViewModels
         {
             Plant.Tick(random.Next(10));
 
+        }
+
+        private void WaterTimer_Tick(object sender, EventArgs e)
+        {
+            canWater = true; // Снова разрешаем полив после задержки
+            waterTimer.Stop(); // Останавливаем таймер
+
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        private void WaterPlant()
+        {
+            if (canWater)
+            {
+                Plant.Water();
+                canWater = false; // Блокируем возможность повторного полива
+                waterTimer.Start(); // Запускаем таймер задержки
+            }
         }
 
         protected void OnPropertyChanged(string name)
